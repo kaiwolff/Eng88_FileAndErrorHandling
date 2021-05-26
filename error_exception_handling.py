f1cc17a49b977d88b744d2efc5d2a11966c0d61d
unknown <kai.wolff.92@gmail.com>
2021-05-26 12:03:52 +0100

    added soem file handling exercises

--- error_exception_handling.py
+++ error_exception_handling.py
@@ -14,12 +14,51 @@
 
 
 #Let's try the same scenario, but with a try except block
+#
+# try:
+#     file = open("order.txt")
+# except FileNotFoundError as errmsg:
+#     #creating aliases. The as portion defines the error message as a variable
+#     print("order.txt not found\n" + str(errmsg))
+# finally:
+#     print("Thank you for visiting, hope to see you again!")
+#     #This will run, regardless of whether the other blocks run.
 
-try:
-    file = open("order.txt")
-except FileNotFoundError as errmsg:
-    #creating aliases. The as portion defines the error message as a variable
-    print("order.txt not found\n" + str(errmsg))
-finally:
-    print("Thank you for visiting, hope to see you again!")
-    #This will run, regardless of whether the other blocks run.
+
+# Second Iteration:
+def open_using_with_and_print(file):
+
+    try:
+        with open("order.txt", "r") as file:
+            for line in file.readlines():
+                print(line.rstrip('\n'))
+
+    except FileNotFoundError as errmsg:
+        print(f"{file} not found\n" + str(errmsg))
+
+    finally:
+        print("Thank you for visiting, hope to see you again!")
+        #This will run, regardless of whether the other blocks run.
+
+
+print(open_using_with_and_print("order.txt"))
+
+#Mini-Task: Create a function called open_with_to_write_to_file to write/add/append
+#display the data with the added items - item names - Pizza, Cake, Avocado, Biryani, Pasta
+
+def open_with_to_write_to_file(file, item_list):
+    try:
+        with open("order.txt", "a") as file:
+            for item in item_list:
+                file.write(f"\n{item}")
+
+    except FileNotFoundError as errmsg:
+        print(f"{file} not found\n" + str(errmsg))
+
+    finally:
+        print("Thank you for adding to the order, hope to see you again!")
+        #This will run, regardless of whether the other blocks run.
+
+order_list = ["Pizza", "Cake", "Avocado", "Biryani", "Pasta"]
+
+open_with_to_write_to_file("order.txt", order_list)
